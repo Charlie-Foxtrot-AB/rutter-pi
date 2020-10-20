71fa8712dfa96cf9139cde8becdba3e1c2a260ab
AdaShoelace <pierre.leidbring@gmail.com>
2020-10-20 16:37:44 +0200

    add ffi-test and hello_world

---
+++ ffi-test/src/lib.rs
@@ -0,0 +1,9 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+#[no_mangle]
+pub extern fn string_from_rust() -> *const c_char {
+    let s = CString::new("Hello World").unwrap();
+    let p = s.as_ptr();
+    std::mem::forget(s);
+    p
+}
