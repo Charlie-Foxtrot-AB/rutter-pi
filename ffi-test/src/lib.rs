daf46a1bad795d7b4ab34dddede29d1ef32b3614
AdaShoelace <pierre.leidbring@gmail.com>
2020-10-26 12:16:24 +0000

    handover

--- ffi-test/src/lib.rs
+++ ffi-test/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
-use rascam::SimpleCamera;
+use rascam::*;
 
 #[no_mangle]
 pub extern fn string_from_rust() -> *const c_char {
@@ -12,11 +12,11 @@ pub extern fn string_from_rust() -> *const c_char {
 
 #[no_mangle]
 pub extern fn take_photo() -> &'static [u8] {
-    [0u8;100] 
+    &[0u8;100] 
 }
 
 #[no_mangle]
-pub extern fn take_photo_and_write_to_disk() -> (&'static [u8], u32) {
+pub extern fn take_photo_and_write_to_disk() -> *const u8 {
  let info = info().unwrap();
     if info.cameras.len() < 1 {
         println!("Found 0 cameras. Exiting");
@@ -24,12 +24,10 @@ pub extern fn take_photo_and_write_to_disk() -> (&'static [u8], u32) {
         ::std::process::exit(1);
     }
     println!("{}", info);
-    let mut camera = SimpleCamera::new(&info.cameras[0]).unwrap();
+    let mut camera = SimpleCamera::new(info.cameras[0].clone()).unwrap();
     camera.activate().unwrap();
     let bytes = camera.take_one().unwrap();
-    let ret_arr_len = bytes.len();
-    let ret_arr_slice = bytes.as_slice();
-    (ret_arr_slice, ret_arr_len)
+    bytes.as_ptr()
     /*File::create("/home/pi/image_from_flutter.jpg").unwrap().write_all(&b).unwrap();
     image_taken()*/
 }
